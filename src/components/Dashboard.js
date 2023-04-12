6a9440103c28178dc5d5ec67cebca04269e6a36a
Deby <44121160+DebyGe@users.noreply.github.com>
2023-04-12 14:29:06 +0200

    Logout & Redirect

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export default function Dashboard() {
+
+  function andleLogOut(e) {
+    e.preventDefault();    
+    localStorage.removeItem('token');
+    window.location.href='/'
+  }
+  
   return(
-    <h2>Dashboard</h2>
+    <div>
+      <h2>Dashboard</h2>
+      <form onSubmit={andleLogOut}>
+        <button type="submit">LogOut</button>
+      </form>
+    </div>
   );
 }
